0cbcfeecfae7c0017590312455425c434f0a1ea5
jrehlaender91 <73409365+jrehlaender91@users.noreply.github.com>
2024-10-31 17:11:44 -0600

    Mobile view
    Mobile view ready - no functionality yet

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
-const bars = document.getElementById('bars');
+Chart.defaults.backgroundColor = '#d5d6ec';
+Chart.defaults.borderColor = '#7477bf';
+Chart.defaults.elements.point.radius = 5;
+//Chart.defaults.plugins.tooltip.cornerRadius = 6;
 
-new Chart(bars, {
-  type: 'bar',
+
+
+const line = document.getElementById('line');
+
+new Chart(line, {
+  type: 'line',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: [
+      '16-22', 
+      '23-29', 
+      '30-5', 
+      '6-12', 
+      '13-19', 
+      '20-26',
+      '27-3',
+      '4-10',
+      '11-17',
+      '18-24',
+      '25-31'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
+      label: 'Traffic',
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1900, 2250, 1500, 2500],
+      borderWidth: 1,
+      fill: true,
+      tension: .5
     }]
   },
   options: {
+    plugins: {
+    legend: {
+    	display: false
+    }},
     scales: {
       y: {
         beginAtZero: true
@@ -19,20 +43,26 @@ new Chart(bars, {
   }
 });
 
-const line = document.getElementById('line');
 
-new Chart(line, {
-  type: 'line',
+const bars = document.getElementById('bars');
+Chart.defaults.backgroundColor = '#7477bf';
+Chart.defaults.borderColor = '#7477bf';
+
+new Chart(bars, {
+  type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
     datasets: [{
-      label: '# of Votes',
-      fill: true,
-      data: [12, 19, 3, 5, 2, 3],
+      label: 'Daily Traffic',
+      data: [70, 120, 175, 125, 225, 200, 100],
       borderWidth: 1
     }]
   },
   options: {
+    plugins: {
+    legend: {
+    	display: false
+    }},
     scales: {
       y: {
         beginAtZero: true
@@ -46,14 +76,23 @@ const pie = document.getElementById('pie');
 new Chart(pie, {
   type: 'doughnut',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Desktop', 'Tablet', 'Phones'],
     datasets: [{
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
+      data: [70, 15, 15],
+      borderWidth: 1,
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ]
     }]
   },
   options: {
+    plugins: {
+    legend: {
+    	display: true
+    }},
     scales: {
       y: {
         beginAtZero: true
