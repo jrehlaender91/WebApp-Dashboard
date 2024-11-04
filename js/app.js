ed1e4422f0229006e9191ebb6c12525aa9ed36b9
jrehlaender91 <73409365+jrehlaender91@users.noreply.github.com>
2024-11-03 23:56:04 -0600

    Final style

--- js/app.js
+++ js/app.js
@@ -1,102 +1,152 @@
-Chart.defaults.backgroundColor = '#d5d6ec';
-Chart.defaults.borderColor = '#7477bf';
+const dailyCanvas = document.getElementById("bars");
+const mobileCanvas = document.getElementById("pie");
+const alertBanner = document.getElementById("alert");
+
+const username = document.getElementById("fname");
+const message = document.getElementById("message");
+const sendButton = document.getElementById("send");
+
+const bell = document.querySelector(".notification");
+const notification = document.querySelector(".notifications-dropdown");
+const circle = document.querySelector(".circle-notification");
+
+alertBanner.innerHTML =
+  `<div class="alert-banner" id="alert">
+    <span class="bold">Alert:</span> You have unread messages <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+  </div>`
+
+alertBanner.addEventListener('click', e => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+  }
+});
+
+bell.addEventListener('click', e => {
+  const element = e.target;
+  circle.style.display = "none";
+    if (notification.style.display === "none") {
+      notification.style.display = "inherit";
+    } else {
+      notification.style.display = "none";
+    }
+    
+    
+});
+
+sendButton.addEventListener('click', e => {
+  const element = e.target;
+  if (username.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (username.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+});
+
 Chart.defaults.elements.point.radius = 5;
-//Chart.defaults.plugins.tooltip.cornerRadius = 6;
 
+let trafficData = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+  "4-10", "11-17", "18-24", "25-31"],
+  datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+    2500],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    tension: .5
+  }]
+};
 
+let trafficOptions = {
+  backgroundColor: 'rgba(112, 104, 201, .5)',
+  fill: true,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    y: {
+    beginAtZero: true
+    }
+  },
+  plugins: {
+    legend: {
+    display: false
+    }
+  }
+};
 
-const line = document.getElementById('line');
-
-new Chart(line, {
+let traffic = new Chart(line, {
   type: 'line',
-  data: {
-    labels: [
-      '16-22', 
-      '23-29', 
-      '30-5', 
-      '6-12', 
-      '13-19', 
-      '20-26',
-      '27-3',
-      '4-10',
-      '11-17',
-      '18-24',
-      '25-31'],
-    datasets: [{
-      label: 'Traffic',
-      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1900, 2250, 1500, 2500],
-      borderWidth: 1,
-      fill: true,
-      tension: .5
+  data: trafficData,
+  options: trafficOptions
+});
+
+
+// data for daily traffic bar chart
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
     }]
-  },
-  options: {
-    plugins: {
-    legend: {
-    	display: false
-    }},
+  };
+
+  const dailyOptions = {
     scales: {
       y: {
         beginAtZero: true
       }
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
     }
-  }
-});
+};  
 
-
-const bars = document.getElementById('bars');
-Chart.defaults.backgroundColor = '#7477bf';
-Chart.defaults.borderColor = '#7477bf';
-
-new Chart(bars, {
+let dailyChart = new Chart(bars, {
   type: 'bar',
-  data: {
-    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-    datasets: [{
-      label: 'Daily Traffic',
-      data: [70, 120, 175, 125, 225, 200, 100],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    plugins: {
+  data: dailyData,
+  options: dailyOptions
+});
+
+
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477BF',
+      '#78CF82',
+      '#51B6C8'
+    ]
+  }]
+};
+
+const mobileOptions = {
+  aspectRatio: 1.9,
+  plugins: {
     legend: {
-    	display: false
-    }},
-    scales: {
-      y: {
-        beginAtZero: true
+      position: 'right',
+      labels: {
+        boxWidth: 20,
+        fontStyle: 'bold'
       }
     }
   }
-});
+};
 
-const pie = document.getElementById('pie');
-
-new Chart(pie, {
+let mobileChart = new Chart(pie, {
   type: 'doughnut',
-  data: {
-    labels: ['Desktop', 'Tablet', 'Phones'],
-    datasets: [{
-      label: '# of Votes',
-      data: [70, 15, 15],
-      borderWidth: 1,
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ]
-    }]
-  },
-  options: {
-    plugins: {
-    legend: {
-    	display: true
-    }},
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+  data: mobileData,
+  options: mobileOptions
+  });
